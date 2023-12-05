384029e4ff05530f1c51f89645ed3f7295decfa1
nickola23 <nikolamilanovic2305@gmail.com>
2023-12-05 13:48:18 +0100

    new class, date and time

--- DatumZaposleni/Form1.cs
+++ DatumZaposleni/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,33 @@ namespace DatumZaposleni
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Zaposleni> lista = new List<Zaposleni>();
+            Zaposleni z = new Zaposleni();
+            z.Ime = textBox1.Text;
+            z.Prezime = textBox2.Text;
+            z.DatumPrijave = dateTimePicker1.Value;
+            z.DatumOdjave = dateTimePicker2.Value;
+            lista.Add(z);
+            textBox3.Text += z.ToString();
+
+            string imeDatoteke = z.Ime.ToLower() + z.Prezime.ToLower();
+            Text = imeDatoteke;
+
+            StreamWriter sw = new StreamWriter(imeDatoteke + ".txt", true);
+            z.pisi(sw);
+            sw.Close();
+            textBox1.Text = string.Empty;
+            textBox2.Text = string.Empty;
+            textBox1.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- DatumZaposleni/Zaposleni.cs
+++ DatumZaposleni/Zaposleni.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,82 @@ namespace DatumZaposleni
 {
     internal class Zaposleni
     {
+        private string ime, prezime;
+        private int godine, meseci, dani;
+        private DateTime datumPrijave, datumOdjave;
+
+        public string Ime
+        {
+            get { return ime; }
+            set { ime = value; }
+        }
+        public string Prezime
+        {
+            get { return prezime; }
+            set { prezime = value; }
+        }
+        public int Godine
+        {
+            get { return godine; }
+            set { godine = value; }
+        }
+        public int Meseci
+        {
+            get { return meseci; }
+            set { meseci = value; }
+        }
+        public int Dani
+        {
+            get { return dani; }
+            set { dani = value; }
+
+        }
+        public DateTime DatumPrijave
+        {
+            get { return datumPrijave; }
+            set { datumPrijave = value; }
+        }
+        public DateTime DatumOdjave
+        {
+            get { return datumOdjave; }
+            set { datumOdjave = value; }
+        }
+        public Zaposleni(){ }
+        public Zaposleni(string ime, string prezime, int godine, int meseci, int dani, DateTime datumPrijave, DateTime datumOdjave)
+        {
+            Ime = ime;
+            Prezime = prezime;
+            Godine = godine;
+            Meseci = meseci;
+            Dani = dani;
+            DatumPrijave = datumPrijave;
+            DatumOdjave = datumOdjave;
+        }
+        public void pisi(StreamWriter sw)
+        {
+            sw.WriteLine("Ime i prezime: " + ime + " " + prezime);
+            sw.WriteLine("Datum prijave: " + datumPrijave.ToShortDateString());
+            sw.WriteLine("Datum odjave: " + datumOdjave.ToShortDateString());
+            sw.WriteLine("===================================================");
+        }
+        public int brDana()
+        {
+            if (datumOdjave > datumPrijave) dani = (datumOdjave - datumPrijave).Days;
+            else dani = 0;
+            return dani;
+        }
+        public string imePrezime()
+        {
+            return "Ime i prezime: " + ime + " " + prezime;
+        }
+        public string brDatum()
+        {
+            return "Datum prijave: " + datumPrijave.ToShortDateString() + "\r\n" + "Datum odjave: " + datumOdjave.ToShortDateString();
+        }
+        public override string ToString()
+        {
+            return imePrezime() + "\r\n" + brDatum() + "\r\n" + " " + brDana() + "\r\n" + "===================================================";
+        }
+
     }
 }
