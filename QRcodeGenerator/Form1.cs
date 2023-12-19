afbcc58d4411f884374d3320f6cd6d7a2c8d4fb6
nickola23 <nikolamilanovic2305@gmail.com>
2023-12-19 13:28:29 +0100

    QRCodeGenerator added

---
+++ QRcodeGenerator/Form1.cs
@@ -0,0 +1,30 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QRcodeGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            QRCodeGenerator qr = new QRCodeGenerator();
+            QRCodeData data = qr.CreateQrCode(textBox1.Text, QRCodeGenerator.ECCLevel.Q);
+            QRCode code = new QRCode(data);
+            pictureBox1.Image = code.GetGraphic(4);
+        }
+    }
+}
