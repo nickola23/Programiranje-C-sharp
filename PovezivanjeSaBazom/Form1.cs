91317181c9bf4dbd1b773e1366a2bd0b74d18fe4
nickola23 <nikolamilanovic2305@gmail.com>
2024-01-23 14:26:20 +0100

    Povezivanje sa bazom

---
+++ PovezivanjeSaBazom/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PovezivanjeSaBazom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Refresh();
+            SqlConnection konekcija = new SqlConnection();
+            konekcija.ConnectionString = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = |DataDirectory|\Radnici_odeljenja_osiguranja1.mdf;Integrated Security=True;Connect Timeout=30";
+            SqlCommand komanda = new SqlCommand();
+            komanda.CommandText = textBox1.Text;
+            komanda.Connection = konekcija;
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = komanda;
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
